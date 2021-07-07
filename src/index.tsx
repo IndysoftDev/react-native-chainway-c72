eb86a0ead3d3a7934d2b0ec869e9b0163abbc723
“Sam <sam.joseph@indysoft.com>
2021-07-07 19:18:40 -0400

    remove multtiply, add init, deinit, and isinit

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,21 @@
 import { NativeModules } from 'react-native';
 
-type ChainwayC72Type = {
-  multiply(a: number, b: number): Promise<number>;
-};
-
 const { ChainwayC72 } = NativeModules;
 
-export default ChainwayC72 as ChainwayC72Type;
+type initReader = () => Promise<boolean>;
+
+type deinitReader = () => Promise<boolean>;
+
+type isReaderInit = () => Promise<boolean>;
+
+const initReader: initReader = () => ChainwayC72.initReader();
+
+const deinitReader: deinitReader = () => ChainwayC72.deinitReader();
+
+const isReaderInit: isReaderInit = () => ChainwayC72.isReaderInit();
+
+export default {
+  initReader,
+  deinitReader,
+  isReaderInit,
+};
