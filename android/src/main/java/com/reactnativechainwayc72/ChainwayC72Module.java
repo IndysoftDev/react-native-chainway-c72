5ab743049f39676f66d60db3199fb4b24dacd229
“Sam <sam.joseph@indysoft.com>
2021-07-07 16:05:02 -0400

    add lifecyle event listener

--- android/src/main/java/com/reactnativechainwayc72/ChainwayC72Module.java
+++ android/src/main/java/com/reactnativechainwayc72/ChainwayC72Module.java
@@ -7,19 +7,36 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.module.annotations.ReactModule;
+import com.facebook.react.bridge.LifecycleEventListener;
+
 
 @ReactModule(name = ChainwayC72Module.NAME)
-public class ChainwayC72Module extends ReactContextBaseJavaModule {
+public class ChainwayC72Module extends ReactContextBaseJavaModule implements LifecycleEventListener {
     public static final String NAME = "ChainwayC72";
+    private final ReactApplicationContext reactContext;
 
     public ChainwayC72Module(ReactApplicationContext reactContext) {
         super(reactContext);
+        this.reactContext = reactContext;
+        this.reactContext.addLifecycleEventListener(this);
     }
 
     @Override
     @NonNull
     public String getName() {
         return NAME;
+    }
+
+    @Override
+    public void onHostDestroy() {
+    }
+
+    @Override
+    public void onHostResume() {
+    }
+
+    @Override
+    public void onHostPause() {
     }
 
 
