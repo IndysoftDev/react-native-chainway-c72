f36e45a48b9414b165645d07c0e4f18b61fa4a22
“Sam <sam.joseph@indysoft.com>
2021-07-13 11:48:31 -0400

    implement scan and find tag functionality

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -7,17 +7,18 @@ import KeyEvent from 'react-native-keyevent';
 export default function App() {
   const [result, setResult] = React.useState<boolean>();
   const [power, setPower] = React.useState<number>();
+  const scanRef = React.useRef<boolean>(false);
 
-  const readTag = async () => {
-    try {
-      const tag = await ChainwayC72.readSingleTag();
-      console.log(tag);
-      return tag;
-    } catch {
-      console.log('missed');
-      return;
-    }
-  };
+  // const readTag = async () => {
+  //   try {
+  //     const tag = await ChainwayC72.readSingleTag();
+  //     console.log(tag);
+  //     return tag;
+  //   } catch {
+  //     console.log('missed');
+  //     return;
+  //   }
+  // };
 
   // const writeToTag = async () => {
   //   try {
@@ -28,6 +29,61 @@ export default function App() {
   //     return;
   //   }
   // };
+
+  const startScanning = () => ChainwayC72.findTag('000000000000000000403238');
+
+  const stopScanning = () => ChainwayC72.stopScan();
+
+  const getProximity = (tag: any) => {
+    const rssi = parseFloat(tag.rssi);
+    if (rssi > -52) {
+      return 100;
+    } else if (rssi > -54) {
+      return 90;
+    } else if (rssi > -56) {
+      return 80;
+    } else if (rssi > -58) {
+      return 70;
+    } else if (rssi > -60) {
+      return 60;
+    } else if (rssi > -62) {
+      return 50;
+    } else if (rssi > -64) {
+      return 40;
+    } else if (rssi > -66) {
+      return 30;
+    } else if (rssi > -68) {
+      return 20;
+    } else if (rssi > -70) {
+      return 10;
+    } else {
+      return 0;
+    }
+  };
+
+  // if (rssi > -50) {
+  //    return 100
+  // } else if rssi > -52 {
+  //     self.readerDelegate?.proximity(proximityPercent: 90)
+  // } else if rssi > -54 {
+  //     self.readerDelegate?.proximity(proximityPercent: 80)
+  // } else if rssi > -56 {
+  //     self.readerDelegate?.proximity(proximityPercent: 70)
+  // } else if rssi > -58 {
+  //     self.readerDelegate?.proximity(proximityPercent: 60)
+  // } else if rssi > -60 {
+  //     self.readerDelegate?.proximity(proximityPercent: 50)
+  // } else if rssi > -62 {
+  //     self.readerDelegate?.proximity(proximityPercent: 40)
+  // } else if rssi > -64 {
+  //     self.readerDelegate?.proximity(proximityPercent: 30)
+  // } else if rssi > -66 {
+  //     self.readerDelegate?.proximity(proximityPercent: 20)
+  // } else if rssi > -68 {
+  //     self.readerDelegate?.proximity(proximityPercent: 10)
+  // } else if rssi <= -68 {
+  //     self.readerDelegate?.proximity(proximityPercent: 0)
+  // }
 
   React.useEffect(() => {
     ChainwayC72.initReader().then((res) => {
@@ -43,8 +99,26 @@ export default function App() {
   }, []);
 
   React.useEffect(() => {
+    ChainwayC72.tagListener((tag) => console.log(getProximity(tag)));
+  }, []);
+
+  React.useEffect(() => {
+    KeyEvent.onKeyDownListener(async () => {
+      if (!scanRef.current) {
+        scanRef.current = true;
+        await startScanning();
+      }
+    });
+
+    return () => KeyEvent.removeKeyDownListener();
+  }, []);
+
+  React.useEffect(() => {
     KeyEvent.onKeyUpListener(async () => {
-      await readTag();
+      if (scanRef.current) {
+        scanRef.current = false;
+        await stopScanning();
+      }
     });
 
     return () => KeyEvent.removeKeyUpListener();
