d384eb75e8f1c141f1f91ac315f06d20bd45d81e
“Sam <sam.joseph@indysoft.com>
2021-10-15 10:55:10 -0400

    update

--- example/src/App.js
+++ example/src/App.js
@@ -1,58 +1,11 @@
 import * as React from 'react';
 
 import { StyleSheet, View, Text } from 'react-native';
-import ChainwayC72 from 'react-native-chainway-c72';
-import KeyEvent from 'react-native-keyevent';
-let lastPress = 0;
 
 export default function App() {
-  const [result, setResult] = React.useState();
-
-  // const readTag = async () => {
-  //   try {
-  //     const tag = await ChainwayC72.readSingleTag();
-  //     console.log(tag);
-  //     return tag;
-  //   } catch {
-  //     console.log('missed');
-  //     return;
-  //   }
-  // };
-
-  React.useEffect(() => {
-    ChainwayC72.initReader().then((res) => {
-      console.log(res);
-      setResult(res);
-    });
-  }, []);
-
-  React.useEffect(() => {
-    ChainwayC72.addTagListener((bc) => console.log(bc));
-
-    return () => ChainwayC72.removeTagListener();
-  }, []);
-
-  React.useEffect(() => {
-    KeyEvent.onKeyUpListener(async () => {
-      await ChainwayC72.stopScan();
-      return;
-    });
-
-    return () => KeyEvent.removeKeyUpListener();
-  }, []);
-
-  React.useEffect(() => {
-    KeyEvent.onKeyDownListener(async () => {
-      await ChainwayC72.startScan();
-      return;
-    });
-
-    return () => KeyEvent.removeKeyDownListener();
-  }, []);
-
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
+      <Text>Result: </Text>
     </View>
   );
 }
